6a67314f3288586347bffbec3674aed028e17023
婧兮Flora <yunyang.zhong@ufl.edu>
2025-02-16 06:17:17 +0800

    Add docstring to get_barycentric_coordinates function.

--- barycentric.py
+++ barycentric.py
@@ -1,6 +1,22 @@
 import numpy as np
 
 def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> np.ndarray:
+    """
+
+    Returns the determinant of a matrix.
+
+    Parameters
+    ----------
+    triangle_coordinates : np.ndarray
+        The coordinates of the triangle.
+    point_coordinates : np.ndarray
+        The coordinates of the point.
+
+    Returns
+    -------
+    np.ndarray, The barycentric coordinates of the triangle.
+
+    """
     x1, x2, x3 = triangle_coordinates[0]
     y1, y2, y3 = triangle_coordinates[1]
     x, y = point_coordinates
