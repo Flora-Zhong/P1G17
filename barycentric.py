2ab7a88580769f89880361cc5c166d18c1856b8a
婧兮Flora <yunyang.zhong@ufl.edu>
2025-02-16 06:11:38 +0800

    get_barycentric_coordinates function completed.

---
+++ barycentric.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> np.ndarray:
+    x1, x2, x3 = triangle_coordinates[0]
+    y1, y2, y3 = triangle_coordinates[1]
+    x, y = point_coordinates
+    left_side = np.array([[x1 - x3, x2 - x3], [y1 - y3, y2 - y3]])
+    right_side = np.array([[x - x3], [y - y3]])
+    lam1, lam2 = np.linalg.solve(left_side, right_side)
+    lam3 = 1 - lam1 - lam2
+    return np.array([lam1, lam2, lam3])
+
