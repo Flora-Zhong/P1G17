27c05e5e8e136a08a5f3310bb810f2d96b786bbb
婧兮Flora <yunyang.zhong@ufl.edu>
2025-02-20 13:12:15 +0800

    Add get_barycentric_coordinates function and get_cartesian_coordiantes to barycentric.py.

--- barycentric.py
+++ barycentric.py
@@ -1,3 +1,55 @@
+import numpy as np
+
+def get_barycentric_coordinates(triangle_coordinates: np.ndarray, point_coordinates: np.ndarray) -> np.ndarray:
+    """
+
+    Returns the barycentric coordinates of the triangle.
+
+    Parameters
+    ----------
+    triangle_coordinates : np.ndarray
+        The coordinates of the triangle.
+    point_coordinates : np.ndarray
+        The coordinates of the point.
+
+    Returns
+    -------
+    np.ndarray, The barycentric coordinates of the triangle.
+
+    """
+    x1, x2, x3 = triangle_coordinates[0]
+    y1, y2, y3 = triangle_coordinates[1]
+    x, y = point_coordinates
+    left_side = np.array([[x1 - x3, x2 - x3], [y1 - y3, y2 - y3]])
+    right_side = np.array([[x - x3], [y - y3]])
+    lam1, lam2 = np.linalg.solve(left_side, right_side)
+    lam3 = 1 - lam1 - lam2
+    return np.array([lam1, lam2, lam3])
+
+def get_cartesian_coordinates(triangle_coordinates: np.ndarray, barycentric_coordinates: np.ndarray) -> np.ndarray:
+    """
+
+    Returns the coordinates of the point.
+
+    Parameters
+    ----------
+    triangle_coordinates : np.ndarray
+        The coordinates of the triangle.
+    barycentric_coordinates : np.ndarray
+        The barycentric coordinates of the triangle.
+
+    Returns
+    -------
+    np.ndarray, The coordinates of the point.
+
+    """
+    x1, x2, x3 = triangle_coordinates[0]
+    y1, y2, y3 = triangle_coordinates[1]
+    lam1, lam2, lam3 = barycentric_coordinates
+    x = lam1 * x1 + lam2 * x2 + lam3 * x3
+    y = lam1 * y1 + lam2 * y2 + lam3 * y3
+    return np.array([x, y])
+
 def is_inside_triangle(triangle_coordinates, point_coordinates):
     barycentric_coord = get_barycentric_coord(triangle_coordinates, point_coordinates)
     return np.all(barycentric_coord >= 0)
