e14e1457ee418b9b0fd062148c6c124d32a8d4d0
elliana :) <elliana.kirby@ufl.edu>
2025-02-20 18:09:02 -0500

    left_endpoint function

---
+++ riemann.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def left_endpoint(x_vals: np.ndarray, func):
+    length = len(x_vals)
+    x_index = 0
+    total = 0
+    for i in range(length - 1):
+        total += func(x_vals[x_index] * (x_vals[x_index + 1] - x_vals[x_index]))
+        x_index += 1
+    return total
+
+def trapezoid(x_vals: np.ndarray, func):
+    return
+
+def simpson(x_vals: np.ndarray, func):
+    return
