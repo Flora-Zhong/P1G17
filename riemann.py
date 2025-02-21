56663e5d86ff4984421123b94a350349de3c7a5b
婧兮Flora <yunyang.zhong@ufl.edu>
2025-02-21 14:35:46 +0800

    Code testing and bug fixing.

--- riemann.py
+++ riemann.py
@@ -17,9 +17,11 @@ def left_endpoint(x_vals: np.ndarray, func: np.ufunc) -> float:
     float, The approximate Riemann integrals with the left endpoint function.
 
     """
-    height = func(x_vals[:-1])
-    width = np.diff(x_vals)
-    total = np.sum(width * height)
+    a = x_vals[:-1]
+    b = x_vals[1:]
+    height = func(a)
+    width = b - a
+    total = np.sum(height * width)
     return total
 
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
@@ -41,9 +43,10 @@ def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     """
     a = x_vals[:-1]
     b = x_vals[1:]
-    width = b - a
     height = 0.5 * ((func(a)) + (func(b)))
-    return np.sum(width * height)
+    width = b - a
+    total = np.sum(height * width)
+    return total
 
 def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
     """
@@ -62,26 +65,7 @@ def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
     float, The approximate Riemann integrals with the simpson function.
 
     """
-    length = len(x_vals)
-    x_index = 0
-    total = 0
-    for i in range(length - 1):
-        mid = (x_vals[x_index] + x_vals[x_index + 1]) / 2
-        total += (x_vals[x_index + 1] - x_vals[x_index]) / 6 * (func(x_vals[x_index]) + 4 * func(mid) + func(x_vals)[x_index + 1])
-        x_index += 1
+    a = x_vals[:-1]
+    b = x_vals[1:]
+    total = np.sum(((b - a) / 6) * (func(a) + 4 * (func((a + b) / 2)) + func(b)))
     return total
-
-
-
-
-x_vals = np.linspace(0, np.pi, 10000)
-func = np.sin
-
-# You don't need to change anything below this line
-left_endpoint_sum = left_endpoint(x_vals, func)
-trapezoid_sum = trapezoid(x_vals, func)
-simpson_sum = simpson(x_vals, func)
-
-print(f"Left Endpoint: {left_endpoint_sum}")
-print(f"    Trapezoid: {trapezoid_sum}")
-print(f"      Simpson: {simpson_sum}")
