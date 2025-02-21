93ff6eecb3356dd25d673564f65d27fa75e40750
婧兮Flora <yunyang.zhong@ufl.edu>
2025-02-21 12:51:02 +0800

    Add docstrings to Part 2, the last time was adding type notations but not docstrings.

--- riemann.py
+++ riemann.py
@@ -1,12 +1,44 @@
 import numpy as np
 
 def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
+    """
+
+    Returns the approximate Riemann integrals with the left endpoint function.
+
+    Parameters
+    ----------
+    x_vals : np.ndarray
+        The x values used in approximating the integral.
+    func : np.ufunc
+        The universal function to approximate the integral.
+
+    Returns
+    -------
+    np.ndarray, The approximate Riemann integrals with the left endpoint function.
+
+    """
     a = x_vals[0]
     b = x_vals[-1]
     total = func(a) * (b - a)
     return total
 
 def trapezoid(x_vals: np.ndarray, func: np.ufunc):
+    """
+
+    Returns the approximate Riemann integrals with the trapezoid function.
+
+    Parameters
+    ----------
+    x_vals : np.ndarray
+        The x values used in approximating the integral.
+    func : np.ufunc
+        The universal function to approximate the integral.
+
+    Returns
+    -------
+    np.ndarray, The approximate Riemann integrals with the trapezoid function.
+
+    """
     length = len(x_vals)
     x_index = 0
     total = 0
@@ -16,6 +48,22 @@ def trapezoid(x_vals: np.ndarray, func: np.ufunc):
     return total
 
 def simpson(x_vals: np.ndarray, func: np.ufunc):
+    """
+
+    Returns the approximate Riemann integrals with the simpson function.
+
+    Parameters
+    ----------
+    x_vals : np.ndarray
+        The x values used in approximating the integral.
+    func : np.ufunc
+        The universal function to approximate the integral.
+
+    Returns
+    -------
+    np.ndarray, The approximate Riemann integrals with the simpson function.
+
+    """
     length = len(x_vals)
     x_index = 0
     total = 0
