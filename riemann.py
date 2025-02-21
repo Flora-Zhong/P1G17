76c6e744db2560b7dc97ecdaed638b076b590294
婧兮Flora <yunyang.zhong@ufl.edu>
2025-02-21 12:22:35 +0800

    Move the trapezoid and simpson function written by Ava to the riemann.py file.

--- riemann.py
+++ riemann.py
@@ -7,7 +7,20 @@ def left_endpoint(x_vals: np.ndarray, func):
     return total
 
 def trapezoid(x_vals: np.ndarray, func):
-    return
+    length = len(x_vals)
+    x_index = 0
+    total = 0
+    for i in range(length - 1):
+        total += (func(x_vals[x_index]) + func(x_vals[x_index + 1])) * (x_vals[x_index + 1] - x_vals[x_index]) / 2
+        x_index += 1
+    return total
 
 def simpson(x_vals: np.ndarray, func):
-    return
+    length = len(x_vals)
+    x_index = 0
+    total = 0
+    for i in range(length - 1):
+        mid = (x_vals[x_index] + x_vals[x_index + 1]) / 2
+        total += (x_vals[x_index + 1] - x_vals[x_index]) / 6 * (func(x_vals[x_index]) + 4 * func(mid) + func(x_vals)[x_index + 1])
+        x_index += 1
+    return total
