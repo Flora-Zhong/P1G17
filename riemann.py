a4bd6e7d13acc83cd1bbe70e96f3113861b899fb
婧兮Flora <yunyang.zhong@ufl.edu>
2025-02-21 12:39:27 +0800

    Add docstring to the func parameters in Part 2.

--- riemann.py
+++ riemann.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def left_endpoint(x_vals: np.ndarray, func):
+def left_endpoint(x_vals: np.ndarray, func: np.ufunc):
     a = x_vals[0]
     b = x_vals[-1]
     total = func(a) * (b - a)
     return total
 
-def trapezoid(x_vals: np.ndarray, func):
+def trapezoid(x_vals: np.ndarray, func: np.ufunc):
     length = len(x_vals)
     x_index = 0
     total = 0
@@ -15,7 +15,7 @@ def trapezoid(x_vals: np.ndarray, func):
         x_index += 1
     return total
 
-def simpson(x_vals: np.ndarray, func):
+def simpson(x_vals: np.ndarray, func: np.ufunc):
     length = len(x_vals)
     x_index = 0
     total = 0
